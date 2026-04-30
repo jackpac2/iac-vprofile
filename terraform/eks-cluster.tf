ec5e6b08eb72ce2cce83ad3c6d886df7b296c12e
Jackpac2 <georylgino@gmail.com>
2026-04-30 18:25:11 +0200

    Added admin permissions for user

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -5,34 +5,31 @@ module "eks" {
   cluster_name    = local.cluster_name
   cluster_version = "1.33"
 
+  enable_cluster_creator_admin_permissions = true
+
   vpc_id                         = module.vpc.vpc_id
   subnet_ids                     = module.vpc.private_subnets
   cluster_endpoint_public_access = true
 
   eks_managed_node_group_defaults = {
     ami_type = "AL2023_x86_64_STANDARD"
-
   }
 
   eks_managed_node_groups = {
     one = {
-      name = "node-group-1"
-
+      name           = "node-group-1"
       instance_types = ["t3.small"]
-
-      min_size     = 1
-      max_size     = 3
-      desired_size = 2
+      min_size       = 1
+      max_size       = 3
+      desired_size   = 2
     }
 
     two = {
-      name = "node-group-2"
-
+      name           = "node-group-2"
       instance_types = ["t3.small"]
-
-      min_size     = 1
-      max_size     = 2
-      desired_size = 1
+      min_size       = 1
+      max_size       = 2
+      desired_size   = 1
     }
   }
 }
